5aa0c9655d851da294101cf8bb71e255e12c235c
Jayla Parrott <jparrot2@terpmail.umd.edu>
2024-05-05 19:01:36 -0400

    took out break and make a goodbye message when the program reaches 3 word guess turns

--- food_bank_guessing_game.py
+++ food_bank_guessing_game.py
@@ -84,10 +84,10 @@ while word_guess_turns <= 3:
         incorrect_letter_bank.append(letter_guess)
         print(f"Incorrect word bank: {incorrect_letter_bank}")
         word_guess_turns += 1
-        break
-
 # scoring for the user
 # combing the incorrect and correct lists for player scoring 
-both_banks = correct_bank + incorrect_letter_bank
-scoring = both_banks.count(letter_guess)
-print(f"Here is the number of times you guessed: {scoring}")
+    both_banks = correct_bank + incorrect_letter_bank
+    scoring = both_banks.count(letter_guess)
+    print(f"Here is the number of times you guessed: {scoring}")
+    print("Hope you had fun!")
+    exit("Exiting the program... Bye Bye!")

f84225e3a8374ffd4369fe5ce87dc03b6499b851
Jayla Parrott <jparrot2@terpmail.umd.edu>
2024-05-11 00:17:29 -0400

    Made a dataframe using pandas using the user's name and their guesses in attempts to get the randomized word

--- food_bank_guessing_game.py
+++ food_bank_guessing_game.py
@@ -2,6 +2,7 @@
 
 # importing random library
 import random
+import pandas as pd
 
 # printing a welcome message to the user 
 print("Welcome to the FOOD WORD GUESSING GAME! (you'll probably end up hungry lol) ")
@@ -17,14 +18,19 @@ categories = list(food_bank.keys())
 # test case to see if categories are printed 
 # print(categories)
 
+# getting the user's name 
+username = input(f"Please enter your username here:  ")
+
+
 # telling the user what the categories are 
-print("""Here are the categories, each filled with words to guess: 
+print(f"""Okay, {username}, Here are the categories, each filled with foods to guess: 
       - Junk
       - Fruits
       - Dairy""")
 
 # randomizing the picking of the categories
 random_choosing = random.choice(categories)
+
 
 # we now need to get the specific values out of the keys 
 values = food_bank.get(random_choosing)
@@ -34,11 +40,25 @@ random_category_word = random.choice(values)
 # print(random_category_word)
 
 # want to mask the randomly slected word, possibly with a function?
-def mask_function(random_category_word, masking_character = "#"): 
+
+def mask_function(random_category_word, masking_character = "_"):
+    """
+    This function will take the randomized word
+    and mask it with the chosen character to 
+    ensure that the word is not revealed to 
+    the user 
+    """
     word_length = len(random_category_word)
     print(f"The length of your word is {word_length} characters.")
     mask = masking_character * word_length
     return mask 
+
+# import this a module and put into another file 
+
+# # tests
+# mask_function("example")
+# mask_function("exam")
+# mask_function("exam", "$")
 
 masked_word = mask_function(random_category_word)
 print(f"AHHH! Your word is from the {random_choosing} category, and here is your word: {masked_word}") 
@@ -90,5 +110,34 @@ while word_guess_turns < 3:
 both_banks = correct_bank + incorrect_letter_bank
 scoring = len(both_banks)
 print(f"Here is the number of times you guessed: {scoring}")
+
+# making an updated dictionary to store the user's name and their final score
+# username and attempts to get the word are the columns 
+user_data = {"Username" : username, 
+             "Attempts to get the word" : scoring}
+user_dataframe = pd.DataFrame([user_data], index=[0])
+print(f"Here is the scoreboard for your attempt to guess the word: \n {user_dataframe}")
 print("Hope you had fun!")
 exit("Exiting the program... Bye Bye!")
+
+
+# making a dictionary for username and their score 
+
+
+
+
+# add some improvements: 
+
+# reveal the secret word after the player fails 
+# control over how many times the letter is guessed
+# more error handling 
+
+
+# advanced topics 
+# make a dictionary for scoring with the players name as the key and the score (using pandas)
+# make a dictionary for player name and scores 
+
+# additional points
+# 
+
+
---
+++ food_game_README.md
@@ -0,0 +1,38 @@
+READ ME FOR FOOD BANK GUESSING GAME:
+
+This is the food bank guessing game with 3 food categories and specific foods for each category. 
+
+This game has the following set of rules:
+- there is one player. 
+- The object of the game is to guess a secret word from a bank of words.
+-  On each turn, the player gets to guess a possible letter that might be in the word.
+- After guessing a letter, the program tells the player how many occurrences of that letter are in
+the secret word.
+- The game should NOT tell the player the position of letters in the word.
+- Optionally on that same turn, the player can also try to guess the word.
+• The program tracks how many letter guesses and how many word guesses each player makes by storing guesses in a correct and incorrect letter list.-
+- A player’s final score is the number of turns (letter guesses) they made before guessing the correct
+word. 
+- There are only 3 word guesses per turn. 
+
+MIT License
+
+Copyright (c) 2024 Jayla Parrott
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
